017b50acd8383137f48c5a8276630e444ee8af55
amoxicillin <10252324+Shadow-Priest@users.noreply.github.com>
2019-07-25 13:23:25 -0700

    hook up wi with yuan-frontend; refactor backend server

---
+++ backend/flaskr/root_router.py
@@ -0,0 +1,23 @@
+import pickle
+
+import rootpath
+from flask import Blueprint, make_response, jsonify
+
+rootpath.append()
+from backend.data_preparation.connection import Connection
+from paths import NLTK_MODEL_PATH
+from backend.classifiers.nltktest import NLTKTest
+
+bp = Blueprint('root', __name__, url_prefix='/')
+nl: NLTKTest = pickle.load(open(NLTK_MODEL_PATH, 'rb'))
+
+
+@bp.route("/wildfire_prediction")
+def send_wildfire():
+    # TODO: update the where clause
+    resp = make_response(
+        jsonify([{"long": lon, "lat": lat, "nlp": nl.predict(text)} for lon, lat, text in Connection().sql_execute(
+            "select l.top_left_long, l.top_left_lat, r.text from locations l, images i, records r where l.id = i.id "
+            "and r.id = l.id and i.wildfire > 40")]))
+
+    return resp
