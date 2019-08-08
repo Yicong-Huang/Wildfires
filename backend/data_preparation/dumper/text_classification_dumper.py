cf2cfc1a957c95692c88c34c5cee8d9cd9978e74
gutingxuan <gutingxuan@outlook.com>
2019-08-08 09:54:51 -0700

    change the names of the column, from text_(not_)wildfire_prob to text_cnn_(not_)wildfire_prob

--- backend/data_preparation/dumper/text_classification_dumper.py
+++ backend/data_preparation/dumper/text_classification_dumper.py
@@ -8,6 +8,6 @@ from backend.data_preparation.connection import Connection
 class TextClassificationDumper(DumperBase):
     def insert(self, id: int, wildfire_prob: float, not_wildfire_prob: float):
         Connection().sql_execute_commit(
-            f"UPDATE records SET text_wildfire_prob = {wildfire_prob}, text_not_wildfire_prob = {not_wildfire_prob} "
+            f"UPDATE records SET text_cnn_wildfire_prob = {wildfire_prob}, text_cnn_not_wildfire_prob = {not_wildfire_prob} "
             f"WHERE id = {id}")
         self.inserted_count += 1
